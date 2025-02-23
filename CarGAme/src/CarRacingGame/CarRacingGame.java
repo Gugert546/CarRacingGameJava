32015255d34c90d033283c0272763b4f4da461d5
Gugert546 <halstensen55@gmail.com>
2025-02-23 12:51:16 +0100

    rydda opp koden

--- CarGAme/src/CarRacingGame/CarRacingGame.java
+++ CarGAme/src/CarRacingGame/CarRacingGame.java
@@ -1,35 +1,21 @@
 package CarRacingGame;
 
-
-
-
-
-
-
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-
-
-
-
-
-
 public class CarRacingGame extends Application {
-    
-        private GameManager gameManager;
 
- @Override
+    private GameManager gameManager;
+
+    @Override
     public void start(Stage primaryStage) {
-    
-        gameManager = new GameManager(primaryStage); 
+        gameManager = new GameManager(primaryStage);
         gameManager.menuPane();
     }
 
-public static void main(String[] args) {
-    Application.launch(args);
+    public static void main(String[] args) {
+        Application.launch(args);
+
+    }
 
 }
-
-}      
-        
--- CarGAme/src/CarRacingGame/Obstacle.java
+++ CarGAme/src/CarRacingGame/Obstacle.java
@@ -1,19 +1,17 @@
 package CarRacingGame;
 
-
-
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import java.util.Random;
 
-
 public class Obstacle extends Rectangle {
-    private static Random random=new Random();
-    private static final double WIDTH = random.nextDouble() * (150 - 25);
+    private static Random random = new Random();
+    private static double WIDTH = random.nextDouble() * (150 - 25);
     private static final double HEIGHT = 50;
-    private static final double SPEED = random.nextDouble() * (10 - 2);
+    private static double SPEED = random.nextDouble() * (10 - 2);
+
     public Obstacle(double x, double y) {
-        super(WIDTH,HEIGHT, Color.RED);
+        super(WIDTH, HEIGHT, Color.RED);
         setX(x);
         setY(y);
     }
@@ -21,6 +19,5 @@ public class Obstacle extends Rectangle {
     public void moveDown() {
         setY(getY() + SPEED);
     }
-
 
 }
