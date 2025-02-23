d812bd18237ffeae88f03311c820ac6757d00ad9
Gugert546 <halstensen55@gmail.com>
2025-02-23 15:39:17 +0100

    dagens arbeid

--- CarGAme/src/CarRacingGame/GameManager.java
+++ CarGAme/src/CarRacingGame/GameManager.java
@@ -5,14 +5,21 @@ import java.util.Iterator;
 import java.util.Random;
 
 import javafx.animation.AnimationTimer;
+import javafx.animation.PathTransition;
+import javafx.animation.Timeline;
+
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class GameManager {
     private Stage primaryStage;
@@ -25,6 +32,7 @@ public class GameManager {
     private int score;
     private Label scoretext;
     private int lives;
+    private Label lifes = new Label();
 
     public GameManager(Stage primaryStage) {
         gamePane = new Pane();
@@ -32,19 +40,19 @@ public class GameManager {
     }
 
     public void menuPane() {
-        // lager en menupane
 
+        // lager en menupane og fjerner tidligere children
         Pane menuPane = new Pane(); // Initialize menuPane
         Scene gamePlay = new Scene(gamePane, 800, 600);
         menuPane.getChildren().clear();
 
         // knapp for å starte spillet
         Button startBt = new Button("Start");
-        startBt.setLayoutX(400);
-        startBt.setLayoutY(500);
+        startBt.setLayoutX(375);
+        startBt.setLayoutY(450);
+
         // håndtering av startknapp
         startBt.setOnAction(e -> {
-
             setupGame();
             primaryStage.setScene(gamePlay);
             primaryStage.show();
@@ -52,25 +60,61 @@ public class GameManager {
             handleKeyPress(gamePlay);
 
         });
-        Rectangle r1 = new Rectangle(0, 0, 800, 600);
-        r1.setFill(Color.RED);
 
+        // bakgrunn, satt til samme farge som gifen
+        Rectangle r1 = new Rectangle(0, 0, 800, 600);
+        r1.setFill(Color.rgb(235, 237, 238));
+
+        // venste hvite rektangle
+        Rectangle r2 = new Rectangle(-20, 200, 300, 200);
+        r2.setArcWidth(30.0);
+        r2.setArcHeight(30.0);
+        r2.setFill(Color.WHITE);
+
+        // høyre hvite rektangle
+        Rectangle r3 = new Rectangle(600, 80, 200, 390);
+        r3.setFill(Color.WHITE);
+        r3.setArcWidth(30.0);
+        r3.setArcHeight(30.0);
+
+        // animert bil
+        Image mocar = new Image(GameManager.class.getResource("/lib/movingcar.gif").toExternalForm());
+        ImageView movingCar = new ImageView(mocar);
+        movingCar.setFitHeight(150);
+        movingCar.setFitWidth(200);
+        if (movingCar != null) { // debugg
+            System.out.println("gif loaded");
+        }
+
+        // path
+        Line l1 = new Line(0, 550, 800, 550);
+
+        // animasjon
+        PathTransition pt = new PathTransition();
+        pt.setDuration(Duration.millis(50000));
+        pt.setNode(movingCar);
+        pt.setPath(l1);
+        pt.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+        pt.setCycleCount(Timeline.INDEFINITE);
+        pt.setAutoReverse(true);
+        pt.play();
+
+        // tittel
         Label tittel = new Label("RacingGame");
-        tittel.setLayoutX(350);
+        tittel.setLayoutX(325);
         tittel.setLayoutY(250);
         tittel.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
 
         // instruksjoner for hvordan spille
-        String instructions = "instruksjoner : bruk piltastene for å styre bilen, unngå hindringene i veien";
+        String instructions = "instruksjoner : bruk piltastene for å styre bilen, unngå hindringene i veien. du får poeng for alle hindringene du kjører forbi";
         Label leftTxt = new Label(instructions);
-        leftTxt.setLayoutX(100);
-        leftTxt.setLayoutY(400);
+        leftTxt.setLayoutX(25);
+        leftTxt.setLayoutY(250);
         leftTxt.setWrapText(true);
         leftTxt.setPrefWidth(175);
 
         // legger til elementene på hovedmenyen
-        menuPane.getChildren().addAll(r1, leftTxt, startBt, tittel);
-
+        menuPane.getChildren().addAll(r1, r2, r3, leftTxt, startBt, tittel, movingCar);
         Scene menu = new Scene(menuPane, 800, 600);
         this.menu = menu;
         primaryStage.setTitle("CarRacingGame");
@@ -109,16 +153,16 @@ public class GameManager {
         // game over beskjed
         Label gameOverLabel = new Label("Game Over! Press 'R' to restart.");
         gameOverLabel.setTextFill(Color.RED);
-        gameOverLabel.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
-        gameOverLabel.setLayoutX(250);
-        gameOverLabel.setLayoutY(100);
+        gameOverLabel.setStyle("-fx-font-size: 32px; -fx-font-weight: bold;");
+        gameOverLabel.setLayoutX(150);
+        gameOverLabel.setLayoutY(300);
 
         Button restartButton = new Button("Restart");
-        restartButton.setLayoutX(350);
-        restartButton.setLayoutY(300);
+        restartButton.setLayoutX(375);
+        restartButton.setLayoutY(450);
         restartButton.setOnAction(e -> restartGame());
-        System.out.println(score);
-        System.out.println(lives);
+        System.out.println("score" + score);
+        System.out.println("hearts" + lives);
         gamePane.getChildren().addAll(gameOverLabel, restartButton);
 
     }
@@ -137,11 +181,11 @@ public class GameManager {
         if (gameLoop != null) {
             gameLoop.stop();
             Button menuBt = new Button("til hovedmenyen");
-            menuBt.setLayoutX(250);
-            menuBt.setLayoutY(500);
+            menuBt.setLayoutX(350);
+            menuBt.setLayoutY(400);
 
             Button resumeBt = new Button("fortsett");
-            resumeBt.setLayoutX(550);
+            resumeBt.setLayoutX(375);
             resumeBt.setLayoutY(500);
 
             resumeBt.setOnAction(e -> {
@@ -160,10 +204,6 @@ public class GameManager {
     }
 
     public void setupGame() {
-        int lives = 3;
-        this.score = 0;
-
-        this.lives = lives;
         gamePane.getChildren().clear();
         // setter bakgrunnsfarge for veien
         Rectangle r1 = new Rectangle(10, 0, 780, 600);
@@ -181,6 +221,12 @@ public class GameManager {
         RoadMarkings.addDottedLine(gamePane);
         // legger til bilen i pane
         gamePane.getChildren().add(car);
+        // legger til liv counter
+        lives = 3;
+        gamePane.getChildren().add(lifes);
+        lifes.setText("liv: " + lives);
+        lifes.setLayoutX(10);
+        lifes.setLayoutY(30);
 
         Label scoretext = new Label();
         this.scoretext = scoretext;
@@ -200,7 +246,6 @@ public class GameManager {
     }
 
     // metode for å oppdatere hindringer
-
     public void updateObstacles() {
         Iterator<Obstacle> iterator = obstacles.iterator();
 
@@ -208,11 +253,11 @@ public class GameManager {
             Obstacle obstacle = iterator.next();
             obstacle.moveDown();
             if (car.getBoundsInParent().intersects(obstacle.getBoundsInParent())) {
+                gamePane.getChildren().remove(obstacle);
+                iterator.remove();
                 lives--;
-                if (lives == 0) {
-                    gameOver();
-                }
-                ;
+                lifes.setText("liv:" + lives);
+                System.out.println(lives);
             }
 
             // fjerner hindringene når de når bunn av skjermen
@@ -223,18 +268,23 @@ public class GameManager {
                 scoretext.setText("" + this.score);
 
             }
+            if (lives == 0) {
+                gameOver();
+            }
         }
     }
 
     // lager en game loop -startes ved trykk på start knappen i hovedmenyen
     public void startGameLoop() {
+        this.lives = 3;
+        this.score = 0;
         AnimationTimer gameLoop = new AnimationTimer() {
             private long lastSpawnTime = 0;
 
             @Override
             public void handle(long now) {
-                if (now - lastSpawnTime > 1_000_000_000) { // Spawn every second to-do endre til en variabel
-                                                           // vanskelighetsgrad
+                if (now - lastSpawnTime > 500_000_000) { // Spawn every second to-do endre til en variabel
+                                                         // vanskelighetsgrad
                     spawnObstacle();
                     lastSpawnTime = now;
                 }
--- CarGAme/src/CarRacingGame/Obstacle.java
+++ CarGAme/src/CarRacingGame/Obstacle.java
@@ -2,13 +2,12 @@ package CarRacingGame;
 
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import java.util.Random;
 
 public class Obstacle extends Rectangle {
-    private static Random random = new Random();
-    private static double WIDTH = random.nextDouble() * (150 - 25);
+
+    private static double WIDTH = 75;
     private static final double HEIGHT = 50;
-    private static double SPEED = random.nextDouble() * (10 - 2);
+    private static double SPEED = 2;
 
     public Obstacle(double x, double y) {
         super(WIDTH, HEIGHT, Color.RED);
